411c56d9a11fc4e233c7ef2a2bf95e177c49578d
George Hickman <george@ghickman.co.uk>
2011-12-03 12:19:44 +0000

    get rid of caching - don't need that yet

--- weather/local_settings.example.py
+++ weather/local_settings.example.py
@@ -1,12 +1,8 @@
-from settings import CACHES
-
 DEBUG = True
 SERVE_MEDIA = DEBUG
 TEMPLATE_DEBUG = DEBUG
 EMAIL_DEBUG = DEBUG
 THUMBNAIL_DEBUG = DEBUG
-
-CACHES['default']['BACKEND'] = 'django.core.cache.backends.dummy.DummyCache'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
