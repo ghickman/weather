d4d4ab957533cdbbef083a598616de12ba4a0abb
Wystan Palm <wystanpalm.oxfam@gmail.com>
2011-12-03 17:15:44 -0800

    Add views and templates for main screens

--- weather/floods/urls.py
+++ weather/floods/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
 
 from floods.views import LandingView
+from floods.views import LocationView
+from floods.views import FloodView
 
 urlpatterns = patterns('',
   url(r'^$', LandingView.as_view(), name='FloodsHome'),
+  url(r'^location/$', LocationView.as_view(), name='LocationInfo'),
+  url(r'^flood/(?P<pk>\d+)$', FloodView.as_view(), name='FloodDetail'),
 )
