1b4c992f7c3e43ff2fe80ce920e9141576f35fb8
George Hickman <george@ghickman.co.uk>
2011-12-04 10:57:57 +0000

    Drop the unused bits of the admin

--- weather/floods/admin.py
+++ weather/floods/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from django.contrib.sites.models import Site
 
 from weather.floods.models import Flood
 
+admin.site.unregister(Group)
+admin.site.unregister(Site)
+admin.site.unregister(User)
 admin.site.register(Flood)
